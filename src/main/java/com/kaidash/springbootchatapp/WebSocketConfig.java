9eb53ef18d3eeca6d70fd9960b1e7bcb38fc1c9d
Dmitriy kaidash <dmitriykaidash1@gmail.com>
2022-09-16 21:19:40 +0200

    fix bag related to annotation

--- src/main/java/com/kaidash/springbootchatapp/WebSocketConfig.java
+++ src/main/java/com/kaidash/springbootchatapp/WebSocketConfig.java
@@ -11,14 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker("/topic");
-    }
-
-    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").withSockJS();
     }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
+    }
 }
